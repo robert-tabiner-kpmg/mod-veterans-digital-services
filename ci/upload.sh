65768a95542ae0dc409fe7205f5702970c6a6d2b
CodeSureUK <55851467+CodeSureUK@users.noreply.github.com>
2021-02-18 13:42:05 +0000

    replaced cf cli with version 7

--- ci/upload.sh
+++ ci/upload.sh
@@ -4,24 +4,25 @@ set -e
 echo "============== INSTALLING CLOUD FOUNDRY CLI CLIENT =============="
 # https://github.com/cloudfoundry/cli/releases
 # wget --max-redirect=1 --output-document=cf_cli_6.26.0.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&version=7.1.0&source=github-rel"
-wget --max-redirect=1 --output-document=cf_cli.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
+# wget --max-redirect=1 --output-document=cf_cli.tgz "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github"
+wget --max-redirect=1 --output-document=cf_cli.tgz https://packages.cloudfoundry.org/stable?release=linux64-binary&version=7.2.0&source=github-rel
 gunzip cf_cli.tgz
 tar -xvf cf_cli.tar
 
 echo "============== LOGGING INTO CLOUD FOUNDRY =============="
 ./cf login -a $CF_API -s $CF_SPACE -o $CF_ORGANIZATION -u $CF_USERNAME -p $CF_PASSWORD
 
-echo "============== SET CF Environment Variables =============="
-./cf set-env blue Email__ApiKey $NOTIFY_API
-./cf set-env blue Email__AFCSTemplateId $AFCS_TEMPLATE_ID
-./cf set-env blue Email__AFCSUserTemplateId $AFCS_USER_TEMPLATE_ID
-./cf set-env blue Email__AFCSEmailRecipient toby@codesure.co.uk
-./cf set-env blue Redis__Uri $REDIS_URL
-
 echo "============== PUSHING CF APP =============="
 # bash ci/zdt.sh
 # export CF_DOCKER_PASSWORD=$DOCKER_PASSWORD
-./cf push blue --docker-image modveterans/forms:dev --docker-username $DOCKER_USERNAME
+./cf push green --docker-image modveterans/forms:dev --docker-username $DOCKER_USERNAME
+
+echo "============== SET CF Environment Variables =============="
+./cf set-env green Email__ApiKey $NOTIFY_API
+./cf set-env green Email__AFCSTemplateId $AFCS_TEMPLATE_ID
+./cf set-env green Email__AFCSUserTemplateId $AFCS_USER_TEMPLATE_ID
+./cf set-env green Email__AFCSEmailRecipient toby@codesure.co.uk
+./cf set-env green Redis__Uri $REDIS_URL
 
 echo "============== BINDING REDIS AND S3 =============="
 ./cf create-service redis tiny-3.2 $REDIS_SERVICE_NAME
